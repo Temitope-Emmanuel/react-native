54f4789247c9e69149c62a66b7fbc48cc9e34ce3
Yhamagee <temitopeojo0@gmail.com>
2021-02-19 13:59:17 +0100

    completed the useDark exercise

--- App.tsx
+++ App.tsx
@@ -1,103 +1,24 @@
 import React from "react"
-import {View,ScrollView,StyleSheet,Keyboard} from "react-native"
-import Header from "./Header"
-import Input from "./TextInput"
-import Button from "./Button"
-import TodoList from "./TodoList"
-import TabBar from "./TabBar"
-
-export interface ITodo {
-  title:string;
-  todoIndex:number;
-  complete:boolean;
-}
-
-export type todoType = "All" | "Completed" | "Active"
+import {Button,StyleSheet,View} from "react-native"
+import getStyleSheet from "./styles"
 
 const App = () => {
+  const [useDark,setUseDark ] = React.useState(false)
 
-  const [todoList,setTodoList] = React.useState<ITodo[]>([])
-  const [currentTodoList,setCurrentTodoList] = React.useState<ITodo[]>([])
-  const [currentTodo,setCurrentTodo] = React.useState("")
-  const [currentTodoType,setCurrentTodoType] = React.useState<todoType>("All")
-  
-  const inputChange = (e: string) => {
-    setCurrentTodo(e)
+  const toggleTheme = () => {
+    console.log("clicking")
+    setUseDark(!useDark)
   }
-  Keyboard.addListener("keyboardDidHide",() => {
-    submitTodo()
-})
-
-  React.useEffect(() => {
-    let newCurrentTodoList:any[];
-    if(currentTodoType === "Active"){
-      newCurrentTodoList = todoList.filter(item => item.complete === false)
-    }else if(currentTodoType === "Completed"){
-      newCurrentTodoList = todoList.filter(item => item.complete === true)
-    }else{
-      newCurrentTodoList = [...todoList]
-    }
-    setCurrentTodoList(newCurrentTodoList)
-  },[currentTodoType,todoList])
-
-  const submitTodo = () => {
-    if(currentTodo.match(/^\s*$/)){
-      return;
-    }
-    const todo = {
-      title:currentTodo,
-      todoIndex:todoList.length+1,
-      complete:false
-    }
-    setTodoList([...todoList,todo])
-    setCurrentTodo("")
-    setCurrentTodoType("All")
-  }
-  const toggleComplete = (arg:number) => {
-    const filteredTodoList = [...todoList]
-    const foundTodoIdx = filteredTodoList.findIndex(item => item.todoIndex === arg)
-    const newTodo = {
-      ...todoList[foundTodoIdx],
-      complete:!todoList[foundTodoIdx].complete
-    }
-    filteredTodoList.splice(foundTodoIdx,1,newTodo)
-    setTodoList([...filteredTodoList])
-  }
-  const handleDelete = (arg:number) => {
-    const filteredTodoList = todoList.filter(item => item.todoIndex !== arg)
-    setTodoList([...filteredTodoList])
-  }
-  const setType = (arg:todoType) => {
-    setCurrentTodoType(arg)
-  }
+  const styles = getStyleSheet(useDark)
+  const backgroundColor = StyleSheet.flatten(styles.container).backgroundColor;
 
   return(
     <View style={styles.container}>
-      <ScrollView keyboardShouldPersistTaps="always" style={styles.content} >
-        <Header/>
-        <Input handleChange={inputChange} submitTodo={submitTodo} value={currentTodo} />
-        <TodoList todos={currentTodoList} handleComplete={toggleComplete}
-         handleDelete={handleDelete} />
-        <Button onClick={submitTodo}>
-          Submit
-        </Button>
-      </ScrollView>
-      <TabBar setType={setType}
-        type={currentTodoType}
-      />
+      <View style={styles.box}>
+        <Button title={backgroundColor} color="black" onPress={toggleTheme}/>
+      </View>
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    backgroundColor:"#f5f5f5"
-  },
-  content:{
-    flex:1,
-    paddingTop:60
-  }
-})
 
 export default App
